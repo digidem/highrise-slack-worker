326e89d93f18f7ca1daf36e0e7171e599769e535
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-01-12 10:53:33 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// @ts-check
+
+/**
+ * Welcome to Cloudflare Workers! This is your first scheduled worker.
+ *
+ * - Run `wrangler dev --local` in your terminal to start a development server
+ * - Run `curl "http://localhost:8787/cdn-cgi/mf/scheduled"` to trigger the scheduled event
+ * - Go back to the console to see what your worker has logged
+ * - Update the Cron trigger in wrangler.toml (see https://developers.cloudflare.com/workers/wrangler/configuration/#triggers)
+ * - Run `wrangler publish --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/runtime-apis/scheduled-event/
+ */
+
+/**
+ * @typedef {object} Env
+ * @property {KVNamespace} HIGHRISE_KV
+ * @property {string} HIGHRISE_TOKEN
+ * @property {string} HIGHRISE_URL
+ * @property {string} SLACK_URL
+ * @property {string} HIGHRISE_GROUPS
+ * @property {string} EVERYONE
+ */
+
+// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+// MY_KV_NAMESPACE: KVNamespace;
+//
+// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+// MY_DURABLE_OBJECT: DurableObjectNamespace;
+//
+// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+// MY_BUCKET: R2Bucket;
+
+export default {
+  /**
+   *
+   * @param {ScheduledController} controller
+   * @param {Env} env
+   * @param {ExecutionContext} ctx
+   */
+  async scheduled (controller, env, ctx) {
+    console.log(`Hello World!`)
+  }
+}
