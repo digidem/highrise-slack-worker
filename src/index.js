5af4ea907e1f7ca33db594153de68d7c5c787b59
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-01-12 22:40:46 +0000

    Change how things work a bit

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const log = debug('highrise-slack:worker')
  * @property {string} SLACK_URL
  * @property {string} HIGHRISE_GROUPS
  * @property {string} EVERYONE
- * @property {string} [START_DATE] initial start date as ISO string in UTC timezone
+ * @property {string} [ENVIRONMENT]
  */
 
 const ONE_WEEK = 1000 * 60 * 60 * 24 * 7
@@ -37,30 +37,14 @@ self.setImmediate = fn => setTimeout(fn, 0)
 /** @param {Env} env */
 async function handleScheduledEvent (env) {
   const syncOptions = parseEnv(env)
-  const key = await encodeKVKey(syncOptions)
+  const key = (env.ENVIRONMENT || 'testing') + 'LastCheck'
   log('KV key for previous sync:', key)
   const previousSync = await env.HIGHRISE_SYNC_KV.get(key)
-  const syncSinceDate = new Date(
-    previousSync || env.START_DATE || Date.now() - ONE_WEEK
-  )
+  const syncSinceDate = new Date(previousSync || Date.now() - ONE_WEEK)
   const syncedUntil = await syncRecordings(syncSinceDate, syncOptions)
   if (syncedUntil.toISOString() !== previousSync) {
     await env.HIGHRISE_SYNC_KV.put(key, syncedUntil.toISOString())
   }
-}
-
-/** @param {Parameters<typeof syncRecordings>[1]} opts */
-async function encodeKVKey (opts) {
-  const textKey =
-    opts.highriseUrl +
-    opts.slackUrl +
-    JSON.stringify(opts.showEveryone) +
-    JSON.stringify(opts.groups.sort())
-  const bufferKey = new TextEncoder().encode(textKey)
-  const hashBuffer = await crypto.subtle.digest('SHA-256', bufferKey)
-  const hashArray = Array.from(new Uint8Array(hashBuffer))
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
-  return hashHex
 }
 
 export default {
